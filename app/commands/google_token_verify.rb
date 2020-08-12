ce4254e0cc4b570626352efd07551efdefadfa21
Actvio <108258577+Actvio@users.noreply.github.com>
2020-08-12 14:14:25 +0000

    Regenerate gemspec for version 1.5.0

--- app/commands/google_token_verify.rb
+++ app/commands/google_token_verify.rb
@@ -11,4 +11,7 @@ class GoogleTokenVerify
 
       user = User.find_or_initialize_by(email: payload['email'])
       if !user.persisted?
-     
+        user = User.create_with_google_info(payload)
+      end
+
+      return {use
