e97d5a49e332f832159ab25e3327427b9fc0c146
Actvio <108258577+Actvio@users.noreply.github.com>
2020-08-21 21:45:19 +0000

    work-in-progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,4 @@
-class ApplicationController < ActionController::AP
+class ApplicationController < ActionController::API
+  before_action :authenticate_request
+  before_action :force_json
+  before_a
