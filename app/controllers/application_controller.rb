44983d87034117de887a49c9bcc3fc812e041d4b
Actvio <108258577+Actvio@users.noreply.github.com>
2020-08-22 21:18:32 +0000

    remove codesponsor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::API
   before_action :force_json
   before_action :set_cors
   attr_reader :current_user
-  # skip_before_action :verify_authenticity_
+  # skip_before_action :verify_authenticity_token
+
+  private
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.heade
