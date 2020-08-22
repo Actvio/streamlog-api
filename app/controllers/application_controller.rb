c70aa79adc3b1d3f7c6fe6f1dc92459456d0e404
Actvio <108258577+Actvio@users.noreply.github.com>
2020-08-22 11:36:58 +0000

    Delete submarine.png

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
   before_action :force_json
-  before_a
+  before_action :set_cors
+  attr_reader :current_user
+  # skip_before_action :verify_authenticity_
