6e544794d6be3a47315e0f3ef37346564511eaf5
Actvio <108258577+Actvio@users.noreply.github.com>
2020-08-29 13:49:30 +0000

    x86: fix boot on uniprocessor systems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,5 @@ class ApplicationController < ActionController::API
   def set_cors
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Ty
