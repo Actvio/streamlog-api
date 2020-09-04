5c6780a2d9a35abd9663715b20bd82ebc231e16a
Actvio <108258577+Actvio@users.noreply.github.com>
2020-09-04 05:41:07 +0000

    Ignore node_modules directory

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,6 @@
 class AuthenticationController < ApplicationController
-  skip_before_action :authenti
+  skip_before_action :authenticate_request
+
+  def login
+    @user = User.find_by(email)
+    authenticate(params[:
