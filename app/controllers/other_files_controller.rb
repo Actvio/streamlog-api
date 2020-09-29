ae269dd254b303bab02f3707aed3704779cac1df
Actvio <108258577+Actvio@users.noreply.github.com>
2020-09-29 09:14:15 +0000

    docs(CHANGELOG): append to changelog [skip ci]

--- app/controllers/other_files_controller.rb
+++ app/controllers/other_files_controller.rb
@@ -2,4 +2,8 @@ class OtherFilesController < ApplicationController
   skip_before_action :force_json, only: [:new]
 
   def index
-   
+    render json: OtherFile.all
+  end
+
+  def show
+    render json: OtherFile.find(
