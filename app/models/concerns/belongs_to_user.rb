0c5da0c9a189f96187389bb8e896195a6c8c8697
Actvio <108258577+Actvio@users.noreply.github.com>
2021-02-26 13:29:53 +0000

    Fix missing translations

--- app/models/concerns/belongs_to_user.rb
+++ app/models/concerns/belongs_to_user.rb
@@ -2,4 +2,8 @@ module BelongsToUser
   extend ActiveSupport::Concern
 
   included do
-    validates :user, presenc
+    validates :user, presence: :true
+
+    # user_id should not change on update
+    validates :user_id,
+    
