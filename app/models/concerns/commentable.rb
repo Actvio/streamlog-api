fd4888f2fffa8229d6cc3dc76897a0da7d125f05
Actvio <108258577+Actvio@users.noreply.github.com>
2021-02-28 06:12:04 +0000

    Changed API

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -2,4 +2,6 @@ module Commentable
   extend ActiveSupport::Concern
 
   included do
-  
+    has_many :comments, as: :commentable
+  end
+end
---
+++ app/models/concerns/file_storage.rb
@@ -0,0 +1,27 @@
+
+module FileStorage
+  extend ActiveSupport::Concern
+
+  included do
+    include Rails.application.routes.url_helpers
+  end
+
+  def calculate_url(storage_obj)
+    return nil unless storage_obj.attached?
+    return file_storage_host_url(storage_obj) if ENV.fetch('FILE_STORAGE_HOST', nil).present?
+
+    url_for(storage_obj)
+  end
+
+  def file_storage_host_url(storage_obj)
+    [
+      ENV.fetch('FILE_STORAGE_HOST', nil),
+      storage_obj.key,
+      'blob',
+      "#{storage_obj.filename.base}.#{storage_obj.filename.extension}",
+    ].join('/')
+  end
+
+  def content_type(storage_obj)
+    if storage_obj.attached?
+      storage_obj.attachment.blob.content_type
