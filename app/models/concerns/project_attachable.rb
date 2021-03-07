3bc5145f628647ac808f618374c3ada3cff305b6
Actvio <108258577+Actvio@users.noreply.github.com>
2021-03-07 20:26:21 +0000

    Enable CI

--- app/models/concerns/project_attachable.rb
+++ app/models/concerns/project_attachable.rb
@@ -2,4 +2,5 @@ module ProjectAttachable
   extend ActiveSupport::Concern
 
   included do
-    has_many :project_attachments, as: :i
+    has_many :project_attachments, as: :item, dependent: :destroy
+    has_many :projects, through: :project_attachments, source: :projec
