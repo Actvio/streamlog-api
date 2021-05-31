b0309f6f1b91c4b7b155d2c1c295186a7fe56b6f
Actvio <108258577+Actvio@users.noreply.github.com>
2021-05-31 21:03:04 +0000

    Update install.sql

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,6 @@
 class Notification < ApplicationRecord
   include BelongsToUser
-  belongs_to :user, i
+  belongs_to :user, inverse_of: :notifications
+  belongs_to :source, polymorphic: true
+
+  validates :message, presence
