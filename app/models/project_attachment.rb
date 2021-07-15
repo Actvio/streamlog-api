d37fb6c9a6f17d23b02e6024d3e960723e3e8039
Actvio <108258577+Actvio@users.noreply.github.com>
2021-07-15 12:41:45 +0000

    lecture9

--- app/models/project_attachment.rb
+++ app/models/project_attachment.rb
@@ -1,4 +1,6 @@
 class ProjectAttachment < ApplicationRecord
   acts_as_paranoid
 
-  belongs_to :project, inverse_of: :proj
+  belongs_to :project, inverse_of: :project_attachments
+  belongs_to :item, polymorphic: true, inverse_of: :project_attachments
+end
