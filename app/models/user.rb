a5efef84faf30740aafa85191355af1a92a243bc
Actvio <108258577+Actvio@users.noreply.github.com>
2021-07-17 13:53:12 +0000

    Delete yarn.lock

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ require 'open-uri'
 
 class User < ApplicationRecord
   include FileStorage
-  has_secure_password validatio
+  has_secure_password validations: false
+  has_one_attached :avatar
+  acts_as_paranoid
+
+  has_many :comments, 
