8d895aa12859ee2dcb673c684beed954d00fc4b5
Actvio <108258577+Actvio@users.noreply.github.com>
2022-01-03 15:47:56 +0000

    added setup script

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails applicatio
+# Initialize the Rails application.
+Rails.application.initialize!
+
+MusicSniperRails::Application.default_url_options = {h
