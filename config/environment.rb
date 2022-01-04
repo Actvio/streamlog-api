f0b8d3f0266df33105393057fba65a49e374570f
Actvio <108258577+Actvio@users.noreply.github.com>
2022-01-04 07:15:00 +0000

    Serialization

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-MusicSniperRails::Application.default_url_options = {h
+MusicSniperRails::Application.default_url_options = {host: ENV.fetch('RAILS_HOST')}
