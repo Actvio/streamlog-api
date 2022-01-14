eb19af34715d2a81372417c3e95d733766513b81
Actvio <108258577+Actvio@users.noreply.github.com>
2022-01-14 17:48:31 +0000

    modify footer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,4 +12,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable/disable cachi
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.joi
