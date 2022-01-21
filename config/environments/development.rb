68cd5ecfbdc4e8506ab843c1ef3c843f96dc1c71
Actvio <108258577+Actvio@users.noreply.github.com>
2022-01-21 19:26:07 +0000

    v1.6.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_s
+  config.active_storage.service = :local
+  # config.active_storage.service = :amazon
+
+  config.read_e
