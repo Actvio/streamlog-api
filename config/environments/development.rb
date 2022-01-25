1f0b5170c1a455c87383e9704dc024e65fe1d791
Actvio <108258577+Actvio@users.noreply.github.com>
2022-01-25 19:15:25 +0000

    position

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Highlight code that triggered
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
+
+  # R
