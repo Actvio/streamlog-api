c6f309f6b7e825c02b6198636dec24a4cec2b592
Actvio <108258577+Actvio@users.noreply.github.com>
2022-02-25 20:57:49 +0000

    automatic BuildDate Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,4 +22,5 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Enable
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.action_con
