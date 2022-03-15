2955c168375a66614dec9072689390f69453e3c9
Actvio <108258577+Actvio@users.noreply.github.com>
2022-03-15 08:13:22 +0000

    post new blog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,4 +32,5 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
 
-  # Mount Ac
+  # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path 
