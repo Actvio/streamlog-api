0bf381e8f76d7e36a74cbca60690a6e1524fdc69
Actvio <108258577+Actvio@users.noreply.github.com>
2022-03-21 13:46:50 +0000

    Improving code quality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,4 +44,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  # Prepend all log lines with the following ta
+  # Prepend all log lines with the following tags.
+  config.log_tags = [ :request_id ]
+
+  # Use a different cache store in produ
