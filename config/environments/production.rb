c9ead697898f75bbfdda49c56f54b20317e8848a
Actvio <108258577+Actvio@users.noreply.github.com>
2022-03-24 11:13:58 +0000

    dev merge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,4 +52,8 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "music-sni
+  # config.active_job.queue_name_prefix = "music-sniper-rails_#{Rails.env}"
+
+  config.action_mailer.perform_caching = false
+
+  # Ignore
