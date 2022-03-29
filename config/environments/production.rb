195d6fdefabeba371fd486c3e54fc2f208c3fa5d
Actvio <108258577+Actvio@users.noreply.github.com>
2022-03-29 19:18:29 +0000

    H

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and 
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
+
+  
