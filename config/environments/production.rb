fd4325a9e4335508c3b9104c112c1986d4f16ced
Actvio <108258577+Actvio@users.noreply.github.com>
2022-03-30 22:55:17 +0000

    conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,4 +72,6 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  # config.logger =
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV["
