f9f24d4482eca611fd681b292281470152ff12e4
Actvio <108258577+Actvio@users.noreply.github.com>
2022-03-31 07:07:39 +0000

    Release v1.1.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.
