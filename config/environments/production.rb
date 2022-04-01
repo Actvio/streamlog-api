ca381918472b8da2aa0eb222361360faf9925fa0
Actvio <108258577+Actvio@users.noreply.github.com>
2022-04-01 09:06:03 +0000

    Delete bootstrap.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSuppor
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  # Do not dump schema after migrations.
