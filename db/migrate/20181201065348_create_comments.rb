34c6fb8a9ca0401ea97661eb1f10827db3e7f88f
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-03 15:30:33 +0000

    updated bin

--- db/migrate/20181201065348_create_comments.rb
+++ db/migrate/20181201065348_create_comments.rb
@@ -1,3 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do
+    create_table :comments do |t|
+      t.text :text
+      t.references :user, index: true
+      t.references :audio_file, 
