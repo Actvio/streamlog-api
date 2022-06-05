0fc41e5b592053fee685ed67089d2775edb0dbed
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-05 23:06:39 +0000

    Minor output update

--- db/migrate/20190802072735_add_deleted_at_to_original_models.rb
+++ db/migrate/20190802072735_add_deleted_at_to_original_models.rb
@@ -2,4 +2,7 @@ class AddDeletedAtToOriginalModels < ActiveRecord::Migration[5.2]
   def change
     add_column :audio_files, :deleted_at, :datetime
     add_column :comments, :deleted_at, :datetime
-    add_column 
+    add_column :users, :deleted_at, :datetime
+
+    add_index :audio_files, :deleted_at
+    add_ind
