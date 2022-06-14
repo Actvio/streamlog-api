5dcae8dc7d20724b596de1c5d4b193900e140d18
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-14 11:09:12 +0000

    Strict types

--- db/migrate/20190813091433_convert_comments_to_polymorphic_commentable.rb
+++ db/migrate/20190813091433_convert_comments_to_polymorphic_commentable.rb
@@ -1,3 +1,4 @@
 class ConvertCommentsToPolymorphicCommentable < ActiveRecord::Migration[5.2]
   def change
-    Comment.f
+    Comment.find_each do |comment|
+      comment.commentable = AudioFile.find_by(id: commen
