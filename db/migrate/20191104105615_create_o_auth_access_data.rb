7ec69cde5267a733e2ac44fbb6a7709e3e4b7678
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-15 22:40:27 +0000

    some instructions

--- db/migrate/20191104105615_create_o_auth_access_data.rb
+++ db/migrate/20191104105615_create_o_auth_access_data.rb
@@ -1,3 +1,5 @@
 class CreateOAuthAccessData < ActiveRecord::Migration[5.2]
   def change
-    create_table :o_auth
+    create_table :o_auth_access_data do |t|
+      t.references :user, index: true
+      t.string :provider, index: 
