eee84dc1ce164223bf3837844a1a25b392ad8bb0
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-16 11:32:08 +0000

    Bump to 0.2.0.

--- db/migrate/20191104105615_create_o_auth_access_data.rb
+++ db/migrate/20191104105615_create_o_auth_access_data.rb
@@ -2,4 +2,9 @@ class CreateOAuthAccessData < ActiveRecord::Migration[5.2]
   def change
     create_table :o_auth_access_data do |t|
       t.references :user, index: true
-      t.string :provider, index: 
+      t.string :provider, index: true
+      t.string :token
+      t.string :refresh_token
+      t.timestamp :expires_at
+
+  
