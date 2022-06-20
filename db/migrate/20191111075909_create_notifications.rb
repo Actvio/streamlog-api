ad6acb2093b235e5f5294d2988cec57bd8e83b8e
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-20 13:00:35 +0000

    merged from funtoo-staging

--- db/migrate/20191111075909_create_notifications.rb
+++ db/migrate/20191111075909_create_notifications.rb
@@ -1,1 +1,4 @@
-class CreateNotifications < ActiveRecord::M
+class CreateNotifications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notifications do |t|
+      t.boolean :marked_read, defau
