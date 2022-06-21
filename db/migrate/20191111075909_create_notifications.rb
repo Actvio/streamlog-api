5d1812f40fd396f9b212678f275c528e7d0cd67f
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-21 21:15:14 +0000

    v0.5.9.7

--- db/migrate/20191111075909_create_notifications.rb
+++ db/migrate/20191111075909_create_notifications.rb
@@ -2,4 +2,6 @@ class CreateNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :notifications do |t|
       t.boolean :marked_read, default: false, index: true
-      t.boolean :email_notified, default: false, index: t
+      t.boolean :email_notified, default: false, index: true
+      t.references :user, index: true
+      t.references :source, polymor
