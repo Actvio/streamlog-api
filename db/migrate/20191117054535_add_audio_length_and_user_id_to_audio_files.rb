8726a8382fd0938039b95d98e4a00936166ae173
Actvio <108258577+Actvio@users.noreply.github.com>
2022-06-29 06:20:50 +0000

    Bump to 0.2.7

--- db/migrate/20191117054535_add_audio_length_and_user_id_to_audio_files.rb
+++ db/migrate/20191117054535_add_audio_length_and_user_id_to_audio_files.rb
@@ -3,4 +3,6 @@ class AddAudioLengthAndUserIdToAudioFiles < ActiveRecord::Migration[5.2]
     add_column :audio_files, :audio_length, :float
 
     add_column :audio_files, :user_id, :bigint
-    add_in
+    add_index :audio_files, :user_id
+  end
+end
