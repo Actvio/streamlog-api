175abbe55baa91b64d7c98dd19f486ea783073e2
Actvio <108258577+Actvio@users.noreply.github.com>
2022-07-08 17:10:53 +0000

    Update bls.html

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,7 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :c
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "
