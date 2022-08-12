724e7e1b2e60eb607a2dff5fc117993731d42089
Actvio <108258577+Actvio@users.noreply.github.com>
2022-08-12 08:09:42 +0000

    first steps

--- db/schema.rb
+++ db/schema.rb
@@ -95,4 +95,5 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["del
+    t.index ["deleted_at"], name: "index_folders_on_deleted_at"
+    t.index ["parent_id"], name: "index_folders_on_pa
