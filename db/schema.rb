1cf7d8b0a266f4dbbc0f7b5505ff817dc5b3484f
Actvio <108258577+Actvio@users.noreply.github.com>
2022-08-23 10:17:55 +0000

    more tuning

--- db/schema.rb
+++ db/schema.rb
@@ -126,4 +126,7 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["provider"], name: "index_o_auth_access_data_on_provider"
-  
+    t.index ["user_id"], name: "index_o_auth_access_data_on_user_id"
+  end
+
+  create_table "oth
