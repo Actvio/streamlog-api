32f791a6d973d4c3a33937da1ffbe294dc9c88c6
Actvio <108258577+Actvio@users.noreply.github.com>
2022-08-28 17:23:33 +0000

    Some fix

--- db/schema.rb
+++ db/schema.rb
@@ -136,4 +136,7 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["deleted_at"], name: "index_other_files_on_deleted_at"
-    t.index ["user_
+    t.index ["user_id"], name: "index_other_files_on_user_id"
+  end
+
+  create_table "project_attachments
