2e0165219a84da53b578aed786618b7adcd2d52f
Actvio <108258577+Actvio@users.noreply.github.com>
2022-08-30 22:13:14 +0000

    Automatic merge from development to default.

--- db/schema.rb
+++ db/schema.rb
@@ -145,4 +145,5 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
     t.bigint "project_id"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
-  
+    t.datetime "updated_at", null: false
+    t.index ["deleted_at"], name: "index_proje
