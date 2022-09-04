2cd767e8c269cccea5339d56a14bacff3daab94b
Actvio <108258577+Actvio@users.noreply.github.com>
2022-09-04 07:39:48 +0000

    Added changelog.

--- db/schema.rb
+++ db/schema.rb
@@ -157,4 +157,5 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
     t.bigint "user_id"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
-    t.datetime "updated
+    t.datetime "updated_at", null: false
+    t.index ["deleted_at"], name: "index_projects_on_delet
