48707f5234dbc54a73fafbc55fdb6c4580dc5275
Actvio <108258577+Actvio@users.noreply.github.com>
2022-09-06 17:58:22 +0000

    release 0.8.1

--- db/schema.rb
+++ db/schema.rb
@@ -164,4 +164,8 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "passw
+    t.string "password_digest"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+   
