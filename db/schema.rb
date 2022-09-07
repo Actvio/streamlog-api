370e9a97c59c554091fa581ee9e216e8de20a215
Actvio <108258577+Actvio@users.noreply.github.com>
2022-09-07 08:46:58 +0000

    usb: core: don't try to reset_device() a port that got just disconnected

--- db/schema.rb
+++ db/schema.rb
@@ -168,4 +168,5 @@ ActiveRecord::Schema.define(version: 2019_11_17_054535) do
     t.string "email"
     t.string "first_name"
     t.string "last_name"
-   
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: fals
