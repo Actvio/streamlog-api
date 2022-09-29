a9c4845bd6bce08f3e20031723315d5c29028e8d
Actvio <108258577+Actvio@users.noreply.github.com>
2022-09-29 09:47:26 +0000

    clk: sunxi: Support factor clocks with N factor starting not from 0

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,4 +2,5 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payloa
+      JWT.encode(payload, Rails.application.secrets.secret_key_base, 'none')
+      # figure out how to use credentials 
