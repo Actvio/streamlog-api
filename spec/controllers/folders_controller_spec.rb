928905b5250eef3998e0a1a6d36318cb760f8044
Actvio <108258577+Actvio@users.noreply.github.com>
2022-10-08 07:43:48 +0000

    Force update

--- spec/controllers/folders_controller_spec.rb
+++ spec/controllers/folders_controller_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe FoldersController, type: :controller do
   before(:each) do
-    @user = FactoryBot.create
+    @user = FactoryBot.create(:user)
+
+    command = AuthenticateUser.call(@user, true)
+    token = comman
