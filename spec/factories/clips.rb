db47aeec9a813e506f3b4f41aa28f66077602e8f
Actvio <108258577+Actvio@users.noreply.github.com>
2022-11-09 15:13:25 +0000

    upgraded dependencies

--- spec/factories/clips.rb
+++ spec/factories/clips.rb
@@ -2,4 +2,9 @@ FactoryBot.define do
   factory :clip do
     audio_file { FactoryBot.create(:audio_file) }
     user { FactoryBot.create(:user) }
-    na
+    name { 'Example Clip' }
+    start_time { 0 }
+    end_time { 100 }
+  end
+
+end
