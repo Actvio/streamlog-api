43e8d415ac970ae93f6331fa1ab64816a6a5155a
Actvio <108258577+Actvio@users.noreply.github.com>
2022-11-16 16:04:05 +0000

    Updated dependencies to latest; added ncu

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,4 +2,7 @@ FactoryBot.define do
   factory :user do
     username { 's' }
     email { 'some@email.com' }
-    password { 'pass'
+    password { 'pass' }
+    password_confirmation { 'pass' }
+  end
+end
