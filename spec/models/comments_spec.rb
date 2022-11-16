6992df56680417a71fbb539484dff15615d31bbf
Actvio <108258577+Actvio@users.noreply.github.com>
2022-11-16 22:45:17 +0000

    Fix jshint warnings

--- spec/models/comments_spec.rb
+++ spec/models/comments_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Comment, type: :model do
   describe 'audio file comments' do
     it 'should be able to attach to an audio file' do
       user = FactoryBot.create(:user)
-      audio_file = FactoryBot.cre
+      audio_file = FactoryBot.create(:audio_file, user_id: user.id)
+      comment = FactoryBot.create(:comment, user: user, c
