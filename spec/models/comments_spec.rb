aeff8af04502ab0c9c36cd1a41f935913e859c9f
Actvio <108258577+Actvio@users.noreply.github.com>
2022-11-16 18:14:40 +0000

    usb: option: add new zte 3g modem pids to option driver

--- spec/models/comments_spec.rb
+++ spec/models/comments_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'audio file comments' do
-    it 'should be able to
+    it 'should be able to attach to an audio file' do
+      user = FactoryBot.create(:user)
+      audio_file = FactoryBot.cre
