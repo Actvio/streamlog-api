465ccfb94fec73b8d2f376b20ddcf6354f53e2de
Actvio <108258577+Actvio@users.noreply.github.com>
2022-11-18 21:23:58 +0000

    percpu: make pcpu_alloc_chunk() use pcpu_mem_free() instead of kfree()

--- spec/models/comments_spec.rb
+++ spec/models/comments_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Comment, type: :model do
     it 'should be able to attach to an audio file' do
       user = FactoryBot.create(:user)
       audio_file = FactoryBot.create(:audio_file, user_id: user.id)
-      comment = FactoryBot.create(:comment, user: user, c
+      comment = FactoryBot.create(:comment, user: user, commentable: audio_file)
+
+      expect(AudioFile.find(audio_file.id).comments.first.id).to 
