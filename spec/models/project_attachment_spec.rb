68a79ce8bf67b6f6492af01a01bce9c84632a543
Actvio <108258577+Actvio@users.noreply.github.com>
2022-11-22 21:41:43 +0000

    renamed scripts

--- spec/models/project_attachment_spec.rb
+++ spec/models/project_attachment_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ProjectAttachment, type: :model do
-  describe 'add attachments to project'
+  describe 'add attachments to project' do
+    before :each do
+      @project = FactoryBot.create(:project)
+    end
+
+    it 'should be 
