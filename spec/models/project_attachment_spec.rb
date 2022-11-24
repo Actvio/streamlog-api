bf3f43117feb8606a327c7abbe02cd8ee7d8d3e2
Actvio <108258577+Actvio@users.noreply.github.com>
2022-11-24 12:28:01 +0000

    First build

--- spec/models/project_attachment_spec.rb
+++ spec/models/project_attachment_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe ProjectAttachment, type: :model do
 
     it 'should be able to attach clips' do
       clip = FactoryBot.create(:clip)
-      @project.clips.p
+      @project.clips.push(clip)
+
+      p = Project.find(@project.id)
+      expect(p.clips.first.id).to 
