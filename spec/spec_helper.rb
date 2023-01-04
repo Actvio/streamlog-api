09d02e262dc058d2e8d1fb7ed5e452ae1ae51989
Actvio <108258577+Actvio@users.noreply.github.com>
2023-01-04 12:56:56 +0000

    GIT_SILENT made messages (after extraction)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,4 +32,7 @@ RSpec.configure do |config|
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and w
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  e
