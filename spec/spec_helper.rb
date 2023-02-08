215c4203eaf392fe54f01b69e4d18835bbcce868
Actvio <108258577+Actvio@users.noreply.github.com>
2023-02-08 09:21:13 +0000

    Setting up project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -73,4 +73,8 @@ RSpec.configure do |config|
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = 'd
+    config.default_formatter = 'doc'
+  end
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run,
