6732afb879466d83554259d1224abe945918edb0
Actvio <108258577+Actvio@users.noreply.github.com>
2023-02-10 08:34:45 +0000

    [GS] Update regression tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -77,4 +77,8 @@ RSpec.configure do |config|
   end
 
   # Print the 10 slowest examples and example groups at the
-  # end of the spec run,
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+
+  
